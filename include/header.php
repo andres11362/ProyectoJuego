b0815dfd85ee109b74f4656c336bc630fa010c30
andres11362 <agongorita@outlook.com>
2017-06-13 20:46:03 -0500

    firts commit

---
+++ include/header.php
@@ -0,0 +1,13 @@
+<?php
+   echo '<!DOCTYPE html>
+         <html lang="es">
+         <head>
+             <meta charset="UTF-8">
+             <meta name="viewport" content="width=device-width, initial-scale=1.0">
+             <link rel="stylesheet" href="css/bootstrap.min.css">
+             <link href="https://fonts.googleapis.com/css?family=Indie+Flower" rel="stylesheet">
+             <link rel="stylesheet" href="css/style.css">
+             <title>Proyecto Juego</title>
+         </head>
+         <body>';
+?>
---
+++ index.php
@@ -0,0 +1,43 @@
+<?php include 'include/header.php'; ?>
+    <div class="container">
+    <div class="page-header" id="tit-cen">
+        <h1>BIENVENIDO</h1>
+    </div>
+        <div class="col-md-12">
+            <div class="col-md-4 centro">
+               <a href="#" id="r1"></a>
+               <div class="texto" id="azul">
+                  <h3>Rompecabezas</h3>
+               </div>
+            </div>
+            <div class="col-md-4 centro">
+                <a href="#"id="r2"></a>
+                <div class="texto" id="violeta">
+                    <h3>Rompecabezas</h3>
+                </div>
+            </div>
+            <div class="col-md-4 centro">
+                <a href="#" id="ah"></a>
+                <div class="texto" id="rojo">
+                    <h3>Ahorcado</h3>
+                </div>
+            </div>
+        </div>
+            <div class="col-md-12" id="abajo">
+                <div class="col-md-4 centro">
+                    <a href="#" id="dd"></a>
+                    <div class="texto" id="verde">
+                        <h3>Arrastra y coloca</h3>
+                    </div>
+                </div>
+                <div class="col-md-4"></div>
+                <div class="col-md-4 centro">
+                    <a href="#" id="ct"></a>
+                    <div class="texto" id="amarillo">
+                        <h3>Concentrese</h3>
+                    </div>
+                </div>
+            </div>
+    </div>
+    <br>
+<?php include 'include/footer.php'; ?>
